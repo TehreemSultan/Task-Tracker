c72c4bc1644afa266f806694647c73bdf48eb706
TehreemSultan <tehreemsultanali@gmail.com>
2021-02-24 23:35:00 +0500

    Task component(for each task)

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,14 +1,10 @@
-import {FaTimes} from 'react-icons/fa'
+import Task from './Task';
 
 const Tasks = ({tasks, onDelete, onToggle}) => {
     return (
        <>
        {tasks.map((task)=>
-        <div className={`task ${task.reminder?'reminder':''}`} onDoubleClick={()=>onToggle(task.id)}>
-             <h3>{task.text} <FaTimes onClick={()=>onDelete(task.id)} style={{color:'red', cursor:'pointer'}}/></h3>
-             <p>{task.day} </p> 
-        </div>
-         
+         <Task key={task.id} task={task} onToggle={onToggle} onDelete={onDelete}></Task>
        )}
        </>
     )
